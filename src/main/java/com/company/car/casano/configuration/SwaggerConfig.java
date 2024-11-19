3081a0512ef69e90965957dde995ca231893b3ec
DESKTOP-UDD8C75\PC <jack813144@gmail.com>
2024-11-19 21:33:31 +0700

    update

---
+++ src/main/java/com/company/car/casano/configuration/SwaggerConfig.java
@@ -0,0 +1,15 @@
+package com.company.car.casano.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("/api/v1"));
+    }
+}
